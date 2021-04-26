dbde451b9109e71625b503f4c2029fe694df1fbe
aninhaa123 <ana.souza1070@etec.sp.gov.br>
2021-04-26 08:55:46 -0300

    2 files modified

--- app/app.module.ts
+++ app/app.module.ts
@@ -7,6 +7,7 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { MulherMaravilhaPage } from '../pages/filmes/mulher-maravilha';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { TabsPage } from '../pages/tabs/tabs';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    MulherMaravilhaPage
   ],
   imports: [
     BrowserModule,
