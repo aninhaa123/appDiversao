08d37706c4434bf5be2e0830ec403969af299d5c
aninhaa123 <ana.souza1070@etec.sp.gov.br>
2021-04-26 08:52:20 -0300

    2 files added

---
+++ pages/filmes/mulher-maravilha.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-mulher-maravilha',
+  templateUrl: 'mulher-maravilha.html'
+})
+export class MulherMaravilhaPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
