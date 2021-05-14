e5a7f35bcbcc16c118fe00e062eb40205e0eae2a
aninhaa123 <ana.souza1070@etec.sp.gov.br>
2021-05-14 08:24:40 -0300

    1 file modified

--- pages/filmes/filmes.ts
+++ pages/filmes/filmes.ts
@@ -17,6 +17,21 @@ filmes = [
      cartaz:"https://www.themoviedb.org/t/p/w94_and_h141_bestv2/5cF5v8N0wMALMcgEHpBZHyYQZDl.jpg",
      lancamento:"21 de junho de 2013"
    },
+   {
+     nome:"Pets Monstruosos",
+     cartaz:"https://www.themoviedb.org/t/p/w220_and_h330_face/dkokENeY5Ka30BFgWAqk14mbnGs.jpg",
+     lancamento:" 02 de abr de 2021"
+   },
+   {
+     nome:"Demon Slayer: O Trem Infinito",
+     cartaz:"https://www.themoviedb.org/t/p/w220_and_h330_face/m2FNRngyJMyxLatBMJR8pbeG2v.jpg",
+     lancamento:"16 de out de 2020 "
+   },
+   {
+     nome:"Give The Movie",
+     cartaz:"https://www.themoviedb.org/t/p/w220_and_h330_face/fsEq2LddodaHvhs4mTZAaqOV6sR.jpg",
+     lancamento:"22 de agosto de 2020 "
+   },
 ];
   constructor(public navCtrl: NavController) {
 
