57a25106260259844caea5dd4ad33d52e20d1027
aninhaa123 <ana.souza1070@etec.sp.gov.br>
2021-05-17 08:26:17 -0300

    1 file modified

--- pages/filmes/filmes.ts
+++ pages/filmes/filmes.ts
@@ -32,6 +32,21 @@ filmes = [
      cartaz:"https://www.themoviedb.org/t/p/w220_and_h330_face/fsEq2LddodaHvhs4mTZAaqOV6sR.jpg",
      lancamento:"22 de agosto de 2020 "
    },
+    {
+     nome:"Naruto Shippuden: O Filme",
+     cartaz:"https://www.themoviedb.org/t/p/w220_and_h330_face/vaEX9f8ud7iF0G7EoyGxAyRNV1p.jpg",
+     lancamento:"04 de ago de 2007 "
+   },
+   {
+     nome:"A Voz do Silêncio",
+     cartaz:"https://www.themoviedb.org/t/p/w220_and_h330_face/tML79svZV19q9z2CsvjyJr2Y9xZ.jpg",
+     lancamento:"17 de set de 2016"
+   },
+   {
+     nome:"O Hobbit: Uma Jornada Inesperada",
+     cartaz:"https://www.themoviedb.org/t/p/w220_and_h330_face/asuSvb1MDN04HftUFxlp7i6ZHpV.jpg",
+     lancamento:"26 de nov de 2012 "
+   },
 ];
   constructor(public navCtrl: NavController) {
 
